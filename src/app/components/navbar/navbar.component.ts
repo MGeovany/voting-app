234c1becc4e3bd3a075e750325b4dfcd969bd76d
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2024-01-23 09:01:11 -0600

    project initialized

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NavbarComponent {}
